fb98bd5c4ff64a2ab4dde988538e6ff67e228329
Eddie <edwardchen_97@hotmail.com>
2024-07-16 13:29:42 +1000

    deck shuffled function done + it works

--- app.js
+++ app.js
@@ -7,12 +7,39 @@ let choices = [
 let effect = ["Heal 5 HP", "Deal 5 HP"];
 let deck = [];
 
-for (choiceCounter=0; choiceCounter < 25; choiceCounter ++) {
-    for (effectCounter=0; effectCounter < 2; effectCounter++) {
+for (choiceCounter=0; choiceCounter < choices.length; choiceCounter ++) {
+    for (effectCounter=0; effectCounter < effect.length; effectCounter++) {
         deck.push(choices[choiceCounter] + ": " + effect[effectCounter])
     }
     
 }
 
+//this method is called the Fisher Yates shuffle
 
-console.log(deck)
+function shuffle (deck) {
+    
+    for (let i = 0 ; i < deck.length; i ++){
+        // this is a loop that goes through the whole array 
+        let temp = deck[i];
+        //this save the current item to a temp variable 
+        let r = Math.floor(Math.random() * deck.length);
+        //generate a random number in the range of the array 
+        deck[i] = deck[r];
+        //replace the current item with the random item 
+        deck[r] = temp;
+        //replace the random item with the current item as temp 
+    }
+    
+}
+console.log(deck) //this works because the function shuffled hasn't been called
+//so everything is in order
+
+shuffle(deck) //this call the functions to shuffle the deck 
+
+console.log(deck) //this produces a shuffled deck 
+
+
+
+//after shuffled then each player will pick a card until hand = 5 
+
+
