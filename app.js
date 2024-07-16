fba485b7e96def0214b81c2f07a5887a8c4c51fd
Eddie <edwardchen_97@hotmail.com>
2024-07-16 20:32:53 +1000

    updated choices array + did nested nested loop to generate deck

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-let choices = [
-    "Rock", "Rock", "Rock", "Rock" , "Rock", 
-    "Paper", "Paper", "Paper", "Paper","Paper", 
-    "Scissor", "Scissor", "Scissor", "Scissor", "Scissor",
-    "Spock", "Spock", "Spock", "Spock", "Spock",
-    "Lizard", "Lizard", "Lizard", "Lizard", "Lizard"];
+// let choices = [
+//     "Rock", "Rock", "Rock", "Rock" , "Rock", 
+//     "Paper", "Paper", "Paper", "Paper","Paper", 
+//     "Scissor", "Scissor", "Scissor", "Scissor", "Scissor",
+//     "Spock", "Spock", "Spock", "Spock", "Spock",
+//     "Lizard", "Lizard", "Lizard", "Lizard", "Lizard"];
+
+let choices = ["Rock", "Paper", "Scissor", "Spock", "Lizard" ]
 let effect = ["Heal 5 HP", "Deal 5 HP"];
 let deck = [];
 
-for (choiceCounter=0; choiceCounter < choices.length; choiceCounter ++) {
-    for (effectCounter=0; effectCounter < effect.length; effectCounter++) {
-        deck.push(choices[choiceCounter] + ": " + effect[effectCounter])
+for (i =0; i < 5 ; i ++) {
+    for (choiceCounter=0; choiceCounter < choices.length; choiceCounter ++) {
+        for (effectCounter=0; effectCounter < effect.length; effectCounter++) {
+            deck.push(choices[choiceCounter] + ": " + effect[effectCounter])
+        }
     }
-    
 }
+
+console.log(deck)
+
+
 
 //this method is called the Fisher Yates shuffle
 
@@ -31,15 +38,31 @@ function shuffle (deck) {
     }
     
 }
-console.log(deck) //this works because the function shuffled hasn't been called
-//so everything is in order
+// console.log(deck) //this works because the function shuffled hasn't been called
+// //so everything is in order
 
-shuffle(deck) //this call the functions to shuffle the deck 
+// shuffle(deck) //this call the functions to shuffle the deck 
 
-console.log(deck) //this produces a shuffled deck 
+// console.log(deck) //this produces a shuffled deck 
+//after shuffled then each player will pick a card until hand = 5
+
+
+function compare () {
+    if (playerChoice === computerChoice) {
+        msg = `you tied!`
+    } else if (playerChoice === choices[0] && computerChoice === choices[2]) {
+        msg = `you win`
+    } else if (playerChoice === choices[1] && computerChoice === choices[0]) {
+        msg = `you win`
+    } else if (playerChoice === choices[2] && computerChoice === choices[1]) {
+        msg =`you win`
+    } else {
+        msg =`you lose! Try again?`
+    }    
+}
 
 
 
-//after shuffled then each player will pick a card until hand = 5 
+
 
 
